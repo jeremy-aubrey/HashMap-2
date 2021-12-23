e85a5baf5ef70e22a90aa79e8fff26c08dd1013e
jeremy-aubrey <jeremyaubrey@gmail.com>
2021-12-23 13:39:28 -0600

    added header comment

--- src/DuplicateFinder.java
+++ src/DuplicateFinder.java
@@ -1,3 +1,13 @@
+import java.util.*;
+
+/*
+A function that returns the first duplicate encountered in an array of Strings.
+The time complexity is O(N).
+
+Author: Jeremy Aubrey
+Date: 12/23/2021
+Purpose: Practice using HashMaps for increasing efficiency
+ */
 
 public class DuplicateFinder {
 	public static void main(String[] args) {
