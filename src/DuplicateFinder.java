64e770e735c008df8c98d6ad0f82f5cca74a1a7d
jeremy-aubrey <jeremyaubrey@gmail.com>
2021-12-23 13:36:41 -0600

    initial commit with stub

---
+++ src/DuplicateFinder.java
@@ -0,0 +1,11 @@
+
+public class DuplicateFinder {
+	public static void main(String[] args) {
+		String[] stub = {"test"};
+		System.out.println(findDuplicate(stub));
+	}
+	
+	public static String findDuplicate(String[] arr) {
+		return arr[0];
+	}
+}
